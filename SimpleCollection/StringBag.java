886490585bd6d610816f046ff1851dcde3ee39d4
Matthew <mwflickner@gmail.com>
2014-10-07 00:31:51 -0700

    Version 2 of StringBag which uses ArrayList as a part of the private instance data.

--- SimpleCollection/StringBag.java
+++ SimpleCollection/StringBag.java
@@ -1,100 +1,64 @@
 public class StringBag implements SimpleCollection{
 
-	private String[] bag;
+	private java.util.ArrayList<String> bag;
 	private int elements;
-	private boolean isEmpty;
 	private final int DEFAULTSIZE = 100;
 	private int arrayDouble = 1;
 
 	//constuctor of empty array
 	public StringBag(){
-		this.elements = 0;
-		this.isEmpty = true;
-		this.bag = new String[DEFAULTSIZE];
+		this.bag = new java.util.ArrayList<String>(DEFAULTSIZE);
 	}
 	public StringBag(String s){
-		this.elements = 1;
-		this.isEmpty = false;
-		this.bag = new String[DEFAULTSIZE];
-		bag[0] = s;
+		this.bag = new java.util.ArrayList<String>(DEFAULTSIZE);
+		bag.add(s);
 	}
 	public StringBag(int arraySize){
-		this.elements = 0;
-		this.isEmpty = true;
-		this.bag = new String[arraySize];
+		this.bag = new java.util.ArrayList<String>(arraySize);
 	}
 	public StringBag(String[] args){
-		this.isEmpty = args.length == 0;
-		this.bag = new String[args.length];
-		this.elements = args.length;
+		this.bag = new java.util.ArrayList<String>(args.length);
 		for (int i = 0; i < args.length; i++) {
-			this.bag[i] = args[i];
+			this.bag.add(args[i]);
 		}
 	}
 
 	public boolean add ( Object o ){
 		if(o instanceof String){
-			if(this.elements >= this.bag.length){
-				String[] newArray;
-				if (this.bag.length == 0) {
-					newArray = new String[DEFAULTSIZE];
-				} else {
-					newArray = new String[this.bag.length * 2];
-				}
-				for (int i = 0; i < this.elements; i++) {
-					newArray[i] = this.bag[i];
-				}
-				this.bag = newArray;
-			}
-			this.isEmpty = false;
-			this.bag[elements] = (String) o;
-			elements++;
+			bag.add((String) o);
 			return true;
 		}
 		return false;
 	}
 
 	public void clear(){
-		this.elements = 0;
-		this.isEmpty = true;
-		this.bag = new String[DEFAULTSIZE];
+		this.bag = new java.util.ArrayList<String>(DEFAULTSIZE);
 	}
 
 	public boolean contains ( Object o ){
 		if(o instanceof String){
-			for(int i = 0; i < this.elements; i++){
-				if(o.equals(this.bag[i])){
-					return true;
-				}
-			}
+			return this.bag.contains(o);
 		}
 		return false;
 	}
 
 	public boolean isEmpty(){
-		return this.isEmpty;
+		return this.bag.isEmpty();
 	}
 
 	public boolean remove ( Object o ){
 		if(o instanceof String){
-			for (int i = 0; i < elements; i++) {
-				if (o.equals(this.bag[i])) {
-					this.bag[i] = this.bag[elements - 1];
-					this.bag[elements - 1] = null;
-					elements--;
-					return true;
-				}
-			}
+			return this.bag.remove(o);
 		}
 		return false;
 	}
 
 	public int size(){
-		return this.elements;
+		return this.bag.size();
 	}
 
 	public Object[] toArray(){
-		return this.bag;
+		return this.bag.toArray();
 	}
 
 }
