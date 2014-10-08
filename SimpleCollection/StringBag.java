253cd46b9a334312e7292d54c16f532a8ecec590
Matthew <mwflickner@gmail.com>
2014-10-07 21:37:25 -0700

    Version 3 of StringBag which makes StringBag extend ArrayList. Also removed extra files from javadoc compile but not sure if this will remove from git i guess we will see

--- SimpleCollection/StringBag.java
+++ SimpleCollection/StringBag.java
@@ -1,64 +1,43 @@
-public class StringBag implements SimpleCollection{
+public class StringBag extends java.util.ArrayList<Object> implements SimpleCollection{
 
-	private java.util.ArrayList<String> bag;
-	private int elements;
-	private final int DEFAULTSIZE = 100;
-	private int arrayDouble = 1;
-
-	//constuctor of empty array
-	public StringBag(){
-		this.bag = new java.util.ArrayList<String>(DEFAULTSIZE);
-	}
 	public StringBag(String s){
-		this.bag = new java.util.ArrayList<String>(DEFAULTSIZE);
-		bag.add(s);
+		super();
+		this.add(s);
 	}
-	public StringBag(int arraySize){
-		this.bag = new java.util.ArrayList<String>(arraySize);
-	}
+	
 	public StringBag(String[] args){
-		this.bag = new java.util.ArrayList<String>(args.length);
+		super(args.length);
 		for (int i = 0; i < args.length; i++) {
-			this.bag.add(args[i]);
+			this.add(args[i]);
 		}
 	}
 
 	public boolean add ( Object o ){
 		if(o instanceof String){
-			bag.add((String) o);
+			super.add(o);
 			return true;
 		}
 		return false;
 	}
 
-	public void clear(){
-		this.bag.clear();
-	}
 
 	public boolean contains ( Object o ){
 		if(o instanceof String){
-			return this.bag.contains(o);
+			return super.contains(o);
 		}
 		return false;
 	}
 
-	public boolean isEmpty(){
-		return this.bag.isEmpty();
-	}
 
 	public boolean remove ( Object o ){
 		if(o instanceof String){
-			return this.bag.remove(o);
+			return super.remove(o);
 		}
 		return false;
 	}
 
-	public int size(){
-		return this.bag.size();
-	}
+	
 
-	public Object[] toArray(){
-		return this.bag.toArray();
-	}
+	
 
 }
