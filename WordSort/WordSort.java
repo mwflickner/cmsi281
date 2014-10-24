c6e5ddab1e576721244ea12ff2e42318030bb45d
Matthew <mwflickner@gmail.com>
2014-10-24 00:00:03 -0700

    first commit on assignment... this does not work pls don't grade this one rachel I'll have the working one up sometime this weekend

---
+++ WordSort/WordSort.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class WordSort {
+
+	public static void main(String args[]){
+		try{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			String input = stdin.readLine();
+
+			while((input=br.readLine())!=null){
+				input = stdin.readLine();
+			}
+		}catch(IOException io){
+			io.printStackTrace();
+		}
+
+	}
+}
