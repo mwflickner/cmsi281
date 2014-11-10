cf08609afd95f920b56e72013c21cda2c99d26de
mwflickner <mwflickner@gmail.com>
2014-11-10 12:28:59 -0800

    input is taken and then put into an array of strings

--- WordSort/WordSort.java
+++ WordSort/WordSort.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 
 public class WordSort {
 
+	//public String theSort
+
 	public static void main(String args[]){
 		char check;
 
@@ -11,14 +13,64 @@ public class WordSort {
 		try{
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			String input;
+			String input = br.readLine();
+			String[] wordList = new String[input.length()];
+			int wordCount = 0;
+			int wordStart = 0;
+			int wordEnd = 0;
+			String outputString;
+			boolean prevWasLetter= false;
 
-			while((input=br.readLine())!=null){
-				System.out.println(input);
+			//splits input into words
+			for(int i=0; i<input.length(); i++){
+				check = input.charAt(i);
+				if(Character.isLetter(check)){
+					if(!prevWasLetter){
+						wordCount++;
+						wordStart = i;
+					}
+					if(prevWasLetter && i==input.length()-1){
+						wordEnd= i+1;
+						outputString = input.substring(wordStart, wordEnd);
+						wordList[wordCount-1] = outputString;
+						prevWasLetter= false;
+						System.out.println(outputString);
+						System.out.println("Word Count is " + wordCount);
+					}
+
+					prevWasLetter = true;
+				}
+				if(!Character.isLetter(check)){
+					if(prevWasLetter){
+						wordEnd = i;
+						outputString = input.substring(wordStart, wordEnd);
+						wordList[wordCount-1]= outputString;
+						//System.out.println(outputString);
+						//System.out.println("Word Count is " + wordCount);	
+					}
+					prevWasLetter = false;
+				}
+			}
+			String[] shortWordList = new String[wordCount];
+
+			for(int i = 0; i<wordCount; i++){
+				shortWordList[i]=wordList[i];
 			}
 
+			for(int i= 0; i< shortWordList.length; i++){
+				System.out.println("Array index " + i + " is " + shortWordList[i]);
+			}
+				
+
+			
+
+		/*	while((input=br.readLine())!=null){
+				System.out.println(input);
+			}
+		*/
+
 		}catch(IOException io){
-			io.printStackTrace();
+			System.out.println("Sorry it didn't work");
 		}
 
 	}
