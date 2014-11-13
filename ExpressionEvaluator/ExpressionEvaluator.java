8501d4a0c0f30015566d48da6d34531f9853658c
mwflickner <mwflickner@gmail.com>
2014-11-13 15:48:37 -0800

    fixed Expression Evaluator to work properly appears to be working

--- ExpressionEvaluator/ExpressionEvaluator.java
+++ ExpressionEvaluator/ExpressionEvaluator.java
@@ -9,50 +9,45 @@ public class ExpressionEvaluator{
 		return true;
 	}
 
-	public static boolean isOperand(String s){
-		if(s.length() > 1){
+	public static boolean isOperator(String operator){
+		if(operator.equals("+")){
+			return true;
+		}
+		else if(operator.equals("-")){
+			return true;
+		}
+		else if(operator.equals("x")){
+			return true;
+		}
+		else if(operator.equals("/")){
+			return true;
+		}
+		else if(operator.equals("%")){
+			return true;
+		}
+		else{
 			return false;
 		}
-		char op = s.charAt(0);
-		if(isPlus(op)||isMinus(op)||isTimes(op)||isDivide(op)||isMod(op)){
-			return true;
-		}
-		return false;
 
 	}
 
-	public static boolean isPlus(char op){
-		if(op == '+'){
-			return true;
+	public static int operation(String operator, int var1, int var2){
+		if(operator.equals("+")){
+			return var1+var2;
 		}
-		else return false;
-	}
+		else if(operator.equals("-")){
+			return var1-var2;
+		}
+		else if(operator.equals("x")){
+			return var1*var2;
+		}
+		else if(operator.equals("/")){
+			return var1/var2;
+		}
+		else{
+			return var1%var2;
+		}
 
-	public static boolean isMinus(char op){
-		if(op == '-'){
-			return true;
-		}
-		else return false;
-	}
-
-	public static boolean isTimes(char op){
-		if(op == 'x'){
-			return true;
-		}
-		else return false;
-	}
-
-	public static boolean isDivide(char op){
-		if(op == '/'){
-			return true;
-		}
-		else return false;
-	}
-	public static boolean isMod(char op){
-		if(op == '%'){
-			return true;
-		}
-		else return false;
 	}
 	
 	public static void main(String[] args){
@@ -60,38 +55,22 @@ public class ExpressionEvaluator{
 		for (int i = 0; i<args.length; i++){
 			if(isInteger(args[i])){
 				reversePolish.push(Integer.parseInt(args[i]));
+				System.out.println("pushed "+ Integer.parseInt(args[i]));
 			}
-			if(isOperand(args[i])){
-				if(isPlus(args[i].charAt(0))){
-					int val1 = (Integer) reversePolish.pop();
-					int val2 = (Integer) reversePolish.pop();
-					reversePolish.push(val1 + val2);
-				}
-				if(isMinus(args[i].charAt(0))){
-					int val1 = (Integer)reversePolish.pop();
-					int val2 = (Integer)reversePolish.pop();
-					reversePolish.push(val1 - val2);
-				}
-				if(isTimes(args[i].charAt(0))){
-					int val1 = (Integer)reversePolish.pop();
-					int val2 = (Integer)reversePolish.pop();
-					reversePolish.push(val1 * val2);
-				}
-				if(isDivide(args[i].charAt(0))){
-					int val1 = (Integer)reversePolish.pop();
-					int val2 = (Integer)reversePolish.pop();
-					reversePolish.push(val1 / val2);
-				}
-				if(isMod(args[i].charAt(0))){
-					int val1 = (Integer)reversePolish.pop();
-					int val2 = (Integer)reversePolish.pop();
-					reversePolish.push(val1 % val2);
-				}
-
+			else if(isOperator(args[i])){
+				int val1 = (Integer) reversePolish.pop();
+				int val2 = (Integer) reversePolish.pop();
+				int newPush = operation(args[i], val2, val1);
+				System.out.println("New push after operation is " + newPush);
+				reversePolish.push(newPush);
 			}
-
+			
+			else{
+				throw new IllegalArgumentException();
+			}
+		
 		}
-		int output = (Integer)reversePolish.pop();
+		int output = (Integer) reversePolish.pop();
 		System.out.println("Lewandowski!!! Answer is " + output);	
 	}
 
